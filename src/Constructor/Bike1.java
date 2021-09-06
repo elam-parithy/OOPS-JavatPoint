b074290145be79ed74ec0dc54ad22c452722c49f
elamparithym <elamparithym@gmail.com>
2021-09-06 13:44:39 +0530

    constructor package changes

---
+++ src/Constructor/Bike1.java
@@ -0,0 +1,10 @@
+package Constructor;
+
+public class Bike1 {
+	
+	public Bike1() {
+		//creating a default constructor  
+		System.out.println("Bike is created");
+	}
+
+}
---
+++ src/Constructor/Student2.java
@@ -0,0 +1,16 @@
+package Constructor;
+
+public class Student2 {
+
+	  int id;  
+	  String name;  
+	    
+	    //creating a parameterized constructor  
+	    Student2(int i,String n){  
+	    id = i;  
+	    name = n;  
+	    }  
+	    
+	  //method to display the values  
+	    void display(){System.out.println(id+" "+name);}  
+}
